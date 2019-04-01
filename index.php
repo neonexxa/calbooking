050506da6cb5f7c11374d2ba1d05a95bad274af8
neonexxa <firdaus@cyberhax.club>
2019-04-01 23:36:54 +0800

    change auto redirect

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 		return false;	
 	}
 }elseif($uri == '/' ||  $uri == '/calbooking/'){
-	header("Location:  ".$uri."/index.php");
+	header("Location:  ".$uri."index.php");
 }else{
 	require_once __DIR__.'/public/index.php';	
 }
